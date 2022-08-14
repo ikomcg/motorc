3d6ed168a0e42083c68ea30eeb24d7f03e94fda9
Miko <108674498+ikomcg@users.noreply.github.com>
2022-08-14 04:06:44 +0100

    Add files via upload

---
+++ js/comment.js
@@ -0,0 +1,6 @@
+document.getElementById('open-comment').addEventListener('click',function(){
+    document.getElementById('form-comment').style.display = 'flex'
+})
+document.getElementById('close-comment').addEventListener('click',function(){
+    document.getElementById('form-comment').style.display = 'none'
+})
